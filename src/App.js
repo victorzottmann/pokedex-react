6304104f9541667a71e971ffbf3c9338adc498df
victorzottmann <zottmann.victor@gmail.com>
2021-05-14 11:05:02 +1000

    implement simple times clicked button counter to understand class components

--- src/App.js
+++ src/App.js
@@ -4,8 +4,57 @@ import "./App.css";
 
 // component classes are useful because they offer more control in the form of lifecycle hooks
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timesClicked: 0
+    };
+    /* 
+      this line is needed in order to declare regular functions in class 
+      components, otherwise use arrow functions
+    */
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    console.log('component mounted');
+  }
+
+  componentDidUpdate() {
+    console.log('the component updated!');
+  }
+
+  componentWillUnmount() {
+    console.log('component unmounted');
+  }
+
+  shouldComponentUpdate() {
+    if (this.state.timesClicked % 2 === 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  /* 
+    in a regular function the 'this' keyword refers to the function itself,
+    but in an arrow function it refers to the class
+  */
+  // handleClick = () =>
+  handleClick() {
+    this.setState({
+      timesClicked: this.state.timesClicked + 1
+    });
+    console.log(this.state.timesClicked);
+  };
+
   render() {
-    return <p>Hello World</p>;
+    return (
+      <>
+        <h3>Times clicked: {this.state.timesClicked}</h3>
+        <button onClick={this.handleClick}>Click me!</button>
+      </>
+    );
   }
 }
 
